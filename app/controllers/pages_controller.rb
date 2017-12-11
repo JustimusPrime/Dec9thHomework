4bb25e9786e31d58fe3106b517018130f83b5998
JustimusPrime <hirejustin@yahoo.com>
2017-12-10 22:41:57 -0700

    everything added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
   def home
+
   end
 
   def titles
+    @posts = Blog.all
   end
 
   def authors
+    @posts = Blog.all
   end
 
   def subjects
+    @posts = Blog.all
   end
 end
